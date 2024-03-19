81d45057b10ced411c5dac98c46a06743e581a6e
milunusu <milanesaruggiero@gmail.com>
2024-03-19 17:24:18 -0400

    pls

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,33 +1,31 @@
-
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams;
-  const id: any = searchParams.get("id");
-  const idAsNumber = parseInt(id);
+  const searchParams = req.nextUrl.searchParams
+  const id:any = searchParams.get("id")
+  const idAsNumber = parseInt(id)
 
-  const nextId = idAsNumber + 1;
+  const nextId = idAsNumber + 1
 
-  if (idAsNumber === 1) {
-    return new NextResponse(`<!DOCTYPE html><html><head>
-      <title>This is frame 2</title>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmTqP6Ajtshe4xHe42NzSMSfjtUNhyxAdGoa2jLWcGpe8Z%1.jpg" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      <meta property="fc:frame:button1" content="ok so why mediocre" />
-      <meta property="fc:frame:button1:action" content="link" />
-      <meta property="fc:frame:button1:target" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-    </head></html>`);
+  if(idAsNumber === 7){
+      return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame 2</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/7.png" />
+    <meta property="fc:frame:button:1" content="Visit CosmicCowboys.cloud" />
+    <meta property="fc:frame:button:1:action" content="post_redirect" />
+    <meta property="fc:frame:button:2" content="Learn How this was made" />
+    <meta property="fc:frame:button:2:action" content="post_redirect" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
+  </head></html>`);
   } else {
-    return new NextResponse(`<!DOCTYPE html><html><head>
-      <title>This is frame ${id}</title>
-      <meta property="fc:frame" content="idk" />
-      <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPRmhLgJUETG2SDDrNDSiaiztDPF2EWYsbkGFmEzytxq7%${id}.jpg" />
-      <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-      <meta property="fc:frame:button1" content="geez alright" />
-      <meta property="fc:frame:button1:action" content="link" />
-      <meta property="fc:frame:button1:target" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
-    </head></html>`);
+  return new NextResponse(`<!DOCTYPE html><html><head>
+    <title>This is frame ${id}</title>
+    <meta property="fc:frame" content="vNext" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/${id}.png" />
+    <meta property="fc:frame:button:1" content="Next Page" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
+  </head></html>`);
   }
 }
 
@@ -36,4 +34,3 @@ export async function POST(req: NextRequest): Promise<Response> {
 }
 
 export const dynamic = 'force-dynamic';
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams;
-  const id = searchParams.get("id");
-  const idAsNumber = parseInt(id || '1');
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Begin"
+    }
+  ],
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/0.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+});
 
-  const nextId = idAsNumber + 1;
+export const metadata: Metadata = {
+  title: 'Cosmic Cowboys',
+  description: 'A frame telling the story of Cosmic Cowboys',
+  openGraph: {
+    title: 'Cosmic Cowboys',
+    description: 'A frame telling the story of Cosmic Cowboys',
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/0.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
-  const gatewayUrl = process.env.NEXT_PUBLIC_GATEWAY_URL;
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-  let imageUrl;
-  if (idAsNumber === 1) {
-    imageUrl = `${gatewayUrl}/ipfs/QmTqP6Ajtshe4xHe42NzSMSfjtUNhyxAdGoa2jLWcGpe8Z%1.jpg`;
-  } else {
-    imageUrl = `${gatewayUrl}/ipfs/QmPRmhLgJUETG2SDDrNDSiaiztDPF2EWYsbkGFmEzytxq7%${id}.jpg`;
-  }
-
-  return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is frame ${id}</title>
-    <meta property="fc:frame" content="${idAsNumber === 1 ? 'vNext' : 'idk'}" />
-    <meta property="fc:frame:image" content="${imageUrl}" />
-    <meta property="fc:frame:image:aspect_ratio" content="1.91:1" />
-    <meta property="fc:frame:button:1" content="${idAsNumber === 1 ? 'ok so' : 'alrighttt ok'}" />
-    <meta property="fc:frame:post_url" content="${baseUrl}/api/frame?id=${nextId}" />
-  </head></html>`);
+export default function Page() {
+  return (
+    <>
+      <h1>Cosmic Cowboys</h1>
+    </>
+  );
 }
-
-export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
-}
-
-export const dynamic = 'force-dynamic';
-
