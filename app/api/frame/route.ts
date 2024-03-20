3d603539b6aab529fc29b462fd0afbdde5575e1b
milunusu <milanesaruggiero@gmail.com>
2024-03-20 12:19:57 -0400

    end

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,14 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams
-  const id:any = searchParams.get("id")
-  const idAsNumber = parseInt(id)
+  // Log environment variables
+  console.log("NEXT_PUBLIC_BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
+  console.log("NEXT_PUBLIC_GATEWAY_URL:", process.env.NEXT_PUBLIC_GATEWAY_URL);
 
-  const nextId = idAsNumber + 1
+  const searchParams = req.nextUrl.searchParams;
+  const id: any = searchParams.get("id");
+  const idAsNumber = parseInt(id);
 
-  if(idAsNumber === 1){
-      return new NextResponse(`<!DOCTYPE html><html><head>
+  const nextId = idAsNumber + 1;
+
+  if (idAsNumber === 1) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 2</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://indigo-advisory-gorilla-502.mypinata.cloud/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/1.jpg" />
@@ -18,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/frame?id=${2}" />
   </head></html>`);
   } else {
-  return new NextResponse(`<!DOCTYPE html><html><head>
+    return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame 1 ${2}</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://indigo-advisory-gorilla-502.mypinata.cloud/ipfs/QmQGN79J7rwX2NmZdEnyX6ViGWAiSJ6irMCyxFQmC4H2VL/0.jpg" />
